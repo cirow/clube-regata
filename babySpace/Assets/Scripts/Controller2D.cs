903524ad790bd6b7d3c9888573d6e833b2c2d016
Ciro Perez Jr <ciro.perezjr@gmail.com>
2018-01-26 19:32:23 -0200

    OverWorld e Grid básica
    Player, Inputs e grid criadas.

---
+++ babySpace/Assets/Scripts/Controller2D.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller2D : MonoBehaviour {
+
+
+
+    [SerializeField]
+    private float walk_speed;
+
+    private Collider2D playerCollider;
+    private Rigidbody2D playerRigidBody;
+
+	// Use this for initialization
+	void Start () {
+        playerCollider = GetComponent<Collider2D>();
+        playerRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      //  playerRigidBody.velocity = new Vector2(PlayerInput.Instance.MoveX, PlayerInput.Instance.MoveY);
+	}
+    private void FixedUpdate()
+    {
+        playerRigidBody.MovePosition(playerRigidBody.position + (PlayerInput.Instance.MoveDirection * walk_speed * Time.fixedDeltaTime));
+
+
+    }
+}
---
+++ babySpace/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+    public static PlayerInput Instance = null;
+
+    private float moveX;
+    private float moveY;
+	// Use this for initialization
+	void Start () {
+        if(PlayerInput.Instance == null)
+        {
+            PlayerInput.Instance = this;
+        }
+        else
+        {
+            Debug.Log("Trying to instantiate 2 PlayerInput Instances");
+            Destroy(this);
+        }
+
+
+        moveX = 0;
+        moveY = 0;
+	}
+
+    public Vector2 MoveDirection
+    {
+        get
+        {
+            return new Vector3(PlayerInput.Instance.MoveX, PlayerInput.Instance.MoveY);
+        }
+    }
+
+    public float MoveX
+    {
+        get
+        {
+            return moveX;
+        } 
+    }
+    public float MoveY
+    {
+        get
+        {
+            return moveY;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+		moveX = Input.GetAxisRaw("Horizontal");
+        moveY = Input.GetAxisRaw("Vertical");
+
+        //Debug.Log("MoveX: " + moveX);
+        //Debug.Log("MoveY: " + moveY);
+
+
+    }
+}
