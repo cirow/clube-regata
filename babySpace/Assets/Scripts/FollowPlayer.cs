40a24062dffb26fab1b8cf2398cad2d1690d0059
Unknown <juliana.rubenich@gmail.com>
2018-01-26 22:49:34 -0200

    MaisUI
    Mais algumas funcoes de ui

---
+++ babySpace/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+	public GameObject mPlayer;
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 cameraFollowPos = new Vector3(mPlayer.transform.position.x, mPlayer.transform.position.y, -10f);
+		transform.position = cameraFollowPos;
+	}
+}
---
+++ babySpace/Assets/Scripts/IconsManagerUI.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IconsManagerUI : MonoBehaviour {
+
+
+	public Image motorIcon;
+	public Image fuelIcon;
+	public Image mirrorIcon;
+	public Text posTextUI;
+
+	public bool m_gotMotor;
+	public bool m_gotFuel;
+	public bool m_gotMirror;
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start ()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(m_gotMotor)
+		{
+			motorIcon.color = Color.white;
+		}
+		else
+		{
+			motorIcon.color = Color.black;
+		}
+		if (m_gotFuel)
+		{
+			fuelIcon.color = Color.white;
+		}
+		else
+		{
+			fuelIcon.color = Color.black;
+		}
+		if (m_gotMirror)
+		{
+			mirrorIcon.color = Color.white;
+		}
+		else
+		{
+			mirrorIcon.color = Color.black;
+		}
+		posTextUI.text = "Pos: " + (int)player.transform.position.x + " ; " + (int)player.transform.position.y;
+	}
+		
+
+}
