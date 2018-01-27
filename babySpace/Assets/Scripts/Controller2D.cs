73ea009e5d3cb6c7e3bef2b356fba7115039328f
Ciro Perez Jr <ciro.perezjr@gmail.com>
2018-01-26 22:41:11 -0200

    Animação e Pickable object
    Walk tree iniciada, pickable object raiz tbm

--- babySpace/Assets/Scripts/Controller2D.cs
+++ babySpace/Assets/Scripts/Controller2D.cs
@@ -11,11 +11,13 @@ public class Controller2D : MonoBehaviour {
 
     private Collider2D playerCollider;
     private Rigidbody2D playerRigidBody;
+    private Animator anim;
 
 	// Use this for initialization
 	void Start () {
         playerCollider = GetComponent<Collider2D>();
         playerRigidBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -25,7 +27,59 @@ public class Controller2D : MonoBehaviour {
     private void FixedUpdate()
     {
         playerRigidBody.MovePosition(playerRigidBody.position + (PlayerInput.Instance.MoveDirection * walk_speed * Time.fixedDeltaTime));
+        anim.SetFloat("MoveX", PlayerInput.Instance.MoveX);
+        anim.SetFloat("MoveY", PlayerInput.Instance.MoveY);
+    }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "PickUpItem")
+        {
+            PickupItem itemPego = collision.gameObject.GetComponent<PickupItem>();
+
+            switch (itemPego.Item) {
+                case TipoItem.anteta:
+                    PegarAntena(itemPego);
+                    break;
+
+                case TipoItem.capacete:
+                    PegarCapacete(itemPego);
+                    break;
+                case TipoItem.console:
+                    PegarConsole(itemPego);
+                    break;
+                default:
+                    PegarPeça(itemPego);
+                    break;
+
+            }
+            itemPego.BeTaken();
+        }
+    }
+
+    private void PegarPeça(PickupItem item)
+    {
+        Debug.Log("Peguei uma peça carai");
+       // item.BeTaken();
+    }
+
+    private void PegarAntena(PickupItem item)
+    {
+        Debug.Log("Peguei uma antena carai");
+       // item.BeTaken();
+
+    }
+
+    private void PegarCapacete(PickupItem item)
+    {
+        Debug.Log("Peguei um capacete carai");
+      //  item.BeTaken();
+
+    }
+    private void PegarConsole(PickupItem item)
+    {
+        Debug.Log("Peguei um console carai");
+      //  item.BeTaken();
 
     }
 }
---
+++ babySpace/Assets/Scripts/PickupItem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TipoItem {
+    capacete, anteta, console, motor, combustivel, espelho
+}
+
+public class PickupItem : MonoBehaviour {
+
+    [SerializeField]
+    private AudioClip audioEffect;
+    private Collider2D collisionRadius;
+
+    [SerializeField]
+    private TipoItem item;
+
+    // Use this for initialization
+    void Start () {
+        collisionRadius = GetComponent<Collider2D>();
+		
+	}
+
+    public TipoItem Item
+    {
+        get
+        {
+            return item;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void BeTaken()
+    {
+        Debug.Log("I just got taken!");
+        Destroy(gameObject);
+    }
+}
