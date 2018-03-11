ae891eee4e2dc4b60cfbf80ea7dc5101d4201861
Ciro Perez Jr <ciro.perezjr@gmail.com>
2018-03-11 20:05:51 -0300

    Auto stash before merge of "master" and "origin/master"
    Stash do refactor do quest manager

---
+++ babySpace/Assets/Scripts/Managers/CreditsManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		AudioManager.instance.WindAudio(false);
+		AudioManager.instance.InterfAudio(false);
+		AudioManager.instance.VictoryAudio(true);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
